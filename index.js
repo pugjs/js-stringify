7b002badb6adf7400b1d4a5fe6a1a11a95986ef4
Forbes Lindesay <forbes@lindesay.co.uk>
2015-05-17 18:38:00 +0100

    Support stringifying 'undefined'

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function stringify(obj) {
   if (obj instanceof Date) {
     return 'new Date(' + stringify(obj.toISOString()) + ')';
   }
+  if (obj === undefined) {
+    return 'undefined';
+  }
   return JSON.stringify(obj)
              .replace(/\u2028/g, '\\u2028')
              .replace(/\u2029/g, '\\u2029');
