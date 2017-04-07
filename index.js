5c4c402fb53e9a7da44e00a826f5ed0e62fb351d
Andrew Bradley <cspotcode@gmail.com>
2017-04-07 15:30:31 -0400

    Allow pretty-printing JSON output

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = stringify;
-function stringify(obj) {
+function stringify(obj, indentation) {
   if (obj instanceof Date) {
     return 'new Date(' + stringify(obj.toISOString()) + ')';
   }
   if (obj === undefined) {
     return 'undefined';
   }
-  return JSON.stringify(obj)
+  return JSON.stringify(obj, undefined, indentation)
              .replace(/\u2028/g, '\\u2028')
              .replace(/\u2029/g, '\\u2029')
              .replace(/</g, '\\u003C')
